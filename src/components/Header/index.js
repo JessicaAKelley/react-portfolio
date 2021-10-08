0a1977520d018b3009f7ac9ccda94c99de2f287a
JessicaAKelley <kelleyajessica@gmail.com>
2021-10-07 22:37:28 -0500

    Added projects to portfolio

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,22 +1,19 @@
-import React from 'react';
-import Nav from '../../components/Nav'
+import React from "react";
+import Nav from "../../components/Nav";
 
 function Header(props) {
-	const { currentTab, setCurrentTab } = props;
+  const { currentTab, setCurrentTab } = props;
 
-	return (
-		<header>
-			<div>
-				<h2>Jessica's React Portfolio</h2>
-			</div>
-			<div>
-				<Nav
-					currentTab={currentTab}
-					setCurrentTab={setCurrentTab}
-				></Nav>
-			</div>
-		</header>
-	);
+  return (
+    <header>
+      <div>
+        <h2>Jessica's React Portfolio</h2>
+      </div>
+      <div>
+        <Nav currentTab={currentTab} setCurrentTab={setCurrentTab}></Nav>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
--- src/components/Portfolio/projectArr.js
+++ src/components/Portfolio/projectArr.js
@@ -1,40 +1,57 @@
-import movieNight from '../../assets/images/movie-night.png';
-import hearthstoneReviewer from '../../assets/images/hearthstone-reviewer.png';
-import weatherDashboard from '../../assets/images/weather-dashboard.png';
-export default 
-    [
-        {
-        'id': 1,
-        'name': 'Movie Night',
-        'image': movieNight,
-        'github': 'https://github.com/JessicaAKelley/movie-night',
-        'deploy': 'https://xxundeadxxdemon.github.io/movie-night/',
-        'technologies': 'HTML, JavaScript, Bulma, Server side APIs, Local Storage'
-    
-        },
-        {
-        'id': 2,
-        'name': 'Hearthstone Reviewer',
-        'image': hearthstoneReviewer,
-        'github': 'https://github.com/JessicaAKelley/hearthstone_reviewer',
-        'deploy': 'https://hearthstone-reviewer.herokuapp.com/',
-        'technologies': 'Node.js, Express.js, Handlebars, MySQL, Sequelize, dotenv, bcrypt'
-        },
-        {
-        'id': 3,
-        'name': 'Weather Dashboard',
-        'image': weatherDashboard,
-        'github': 'https://github.com/JessicaAKelley/weather-dashboard',
-        'deploy': 'https://jessicaakelley.github.io/weather-dashboard/',
-        'technologies': 'JavaScript, HTML, Bootstrap, Moment, API, Local Storage'
-        },
-        // {
-        // 'id': 5,
-        // 'name': 'Password Generator',
-        // 'image': pwdGenerator,
-        // 'github': 'https://github.com/JessicaAKelley/password-generator',
-        // 'deploy': 'https://jessicaakelley.github.io/password-generator/',
-        // 'technologies': 'HTML, CSS, JavaScript'
-        // },
-        
-    ]
+// import niteEvents from "../../assets/images/nite-events.png";
+import hearthstoneReviewer from "../../assets/images/hearthstone-reviewer.png";
+import movieNight from "../../assets/images/movie-night.png";
+import weatherDashboard from "../../assets/images/weather-dashboard.png";
+import passwordGenerator from "../../assets/images/password-generator.png";
+import workDayScheduler from "../../assets/images/work-day-scheduler.png";
+export default [
+  {
+    id: 1,
+    name: "2Nite Events",
+    //image: 2NiteEvents,
+    github: "https://github.com/JessicaAKelley/password-generator",
+    deploy: "https://jessicaakelley.github.io/password-generator/",
+    technologies: "HTML, CSS, JavaScript",
+  },
+  {
+    id: 2,
+    name: "Hearthstone Reviewer",
+    image: hearthstoneReviewer,
+    github: "https://github.com/JessicaAKelley/hearthstone_reviewer",
+    deploy: "https://hearthstone-reviewer.herokuapp.com/",
+    technologies:
+      "Node.js, Express.js, Handlebars, MySQL, Sequelize, dotenv, bcrypt",
+  },
+  {
+    id: 3,
+    name: "Movie Night",
+    image: movieNight,
+    github: "https://github.com/JessicaAKelley/movie-night",
+    deploy: "https://xxundeadxxdemon.github.io/movie-night/",
+    technologies: "HTML, JavaScript, Bulma, Server side APIs, Local Storage",
+  },
+  {
+    id: 4,
+    name: "Weather Dashboard",
+    image: weatherDashboard,
+    github: "https://github.com/JessicaAKelley/weather-dashboard",
+    deploy: "https://jessicaakelley.github.io/weather-dashboard/",
+    technologies: "JavaScript, HTML, Bootstrap, Moment, API, Local Storage",
+  },
+  {
+    id: 5,
+    name: "Password Generator",
+    image: passwordGenerator,
+    github: "https://github.com/JessicaAKelley/password-generator",
+    deploy: "https://jessicaakelley.github.io/password-generator/",
+    technologies: "HTML, CSS, JavaScript",
+  },
+  {
+    id: 6,
+    name: "Work Day Scheduler",
+    image: workDayScheduler,
+    github: "https://github.com/JessicaAKelley/work-day-scheduler",
+    deploy: "https://jessicaakelley.github.io/work-day-scheduler/",
+    technologies: "HTML, CSS, JavaScript",
+  },
+];
